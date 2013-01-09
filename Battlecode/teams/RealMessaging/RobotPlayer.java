73104d694a7b0ed0e6624697064557f52319ff23
sheep <blacksheepmails@gmail.com>
2013-01-09 16:05:25 -0500

    real messaging done.

---
+++ Battlecode/teams/RealMessaging/RobotPlayer.java
@@ -0,0 +1,37 @@
+package RealMessaging;
+import battlecode.common.Clock;
+import battlecode.common.Direction;
+import battlecode.common.GameConstants;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class RobotPlayer {
+	public static void run(RobotController rc) {
+		Message msg = new Message(rc);
+		while (true) {
+			try {
+				msg.reset();
+				if (rc.getType() == RobotType.HQ) {
+					if (Clock.getRoundNum() < 2) {
+						Direction dir = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+						if (rc.canMove(dir))
+							rc.spawn(dir);
+					} else {
+						msg.send("010010110");
+						msg.sendLoc(rc.senseEnemyHQLocation());
+					}
+				} else if (rc.getType() == RobotType.SOLDIER) {
+					if (rc.isActive()) {
+						System.out.println(msg.receive());
+						System.out.println(msg.receive());
+					}
+				}
+
+				// End turn
+				rc.yield();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
