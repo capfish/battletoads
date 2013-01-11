18ed44d1180b6b54bc981e1b53f12466826bd13d
pomeroy <jon.danger.pomeroy@gmail.com>
2013-01-11 11:57:47 -0500

    slim attitudebot outline

---
+++ Battlecode/teams/attitudebot/RobotPlayer.java
@@ -0,0 +1,37 @@
+package attitudebot;
+
+import battlecode.common.Direction;
+import battlecode.common.GameConstants;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import battlecode.common.Team;
+import battlecode.common.MapLocation;
+import battlecode.common.Clock;
+import battlecode.common.Upgrade;
+
+public class RobotPlayer {
+    public static void run(RobotController rc) {
+        while (true) {
+            try (
+                 Navigation
+
+                 Mine Avoidance
+                 
+                 HQ destruction
+
+                 Mine Laying
+
+                 Researching UpgrayDD
+
+                 Some Encampment Camping
+
+                 Chasing around little bitches
+
+                 Sweet Rallying
+                 
+                 } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+}
