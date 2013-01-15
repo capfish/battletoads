32ba035bc7c730954f02aab0b33ba4f8f5aad3db
pomeroy <jon.danger.pomeroy@gmail.com>
2013-01-15 00:16:09 -0500

    dumbbot less dumb

--- Battlecode/teams/dumbbot/RobotPlayer.java
+++ Battlecode/teams/dumbbot/RobotPlayer.java
@@ -23,27 +23,30 @@ public class RobotPlayer {
                 if (rc.getType() == RobotType.HQ) {
                     dumbbot.hqCode.hqRun(rc, enemyHQ);
 
+                } else if (rc.getType() == RobotType.ARTILLERY) {
+                    dumbbot.encampCode.artilleryRun(rc);
+                    
                 } else if (rc.getType() == RobotType.SOLDIER) {
                     if (rc.isActive()) {
 
                         MapLocation[] neutralMines = rc.senseMineLocations(rc.getLocation(), 2, Team.NEUTRAL);
                         MapLocation[] enemyMines = rc.senseMineLocations(rc.getLocation(), 2, rc.getTeam().opponent());
                         MapLocation[] nearbyEncampments = new MapLocation[]{};
-                        if (Clock.getRoundNum() < 250) {
+                        if (Clock.getRoundNum() < 10000) {
                             nearbyEncampments = rc.senseEncampmentSquares(rc.getLocation(), 16, Team.NEUTRAL);
-                        //    MapLocation[] allEncampments = rc.senseAllEncampmentSquares();
-                        //MapLocation[] myEncampments = rc.senseAlliedEncampmentSquares();
+                            //    MapLocation[] allEncampments = rc.senseAllEncampmentSquares();
+                            //MapLocation[] myEncampments = rc.senseAlliedEncampmentSquares();
                         }
-                                                
+
                         if (enemyMines.length != 0) rc.defuseMine(enemyMines[0]);
                         else if (neutralMines.length != 0) rc.defuseMine(neutralMines[0]);
 
-                        if(Clock.getRoundNum() < 250){
-                            if (Math.random() < .5) {// && rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) < 16) {
+                        if(Clock.getRoundNum() < 10000){
+                            if (Math.random() < .2) {// && rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) < 16) {
                                 // Lay a mine 
                                 if(rc.senseMine(rc.getLocation())==null) rc.layMine();
                             } else if (nearbyEncampments.length != 0) {
-                                if (rc.senseEncampmentSquare(rc.getLocation())) rc.captureEncampment(RobotType.GENERATOR);
+                                if (rc.senseEncampmentSquare(rc.getLocation())) rc.captureEncampment(RobotType.ARTILLERY);
                                 else {
                                     Direction dir = rc.getLocation().directionTo(nearbyEncampments[0]);
                                     if (rc.canMove(dir)) rc.move(dir);
@@ -52,48 +55,44 @@ public class RobotPlayer {
                             } else { 
                                 // Choose a random direction, and move that way if possible
                                 Direction dir = randomDir(rc);
-                                if(rc.canMove(dir)) {
-                                    rc.move(dir);
-                                    rc.setIndicatorString(0, "Last direction moved: "+dir.toString());
-                                }
+                                rc.move(dir); System.out.println("hello3");
                             }
                         }
-                            
-                        
-                        if (Clock.getRoundNum() >= 250){
-                            if (Math.random() < .25) {// && rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) < 16) {
-                                // Lay a mine 
-                                if(rc.senseMine(rc.getLocation())==null)
-                                    rc.layMine();
-                            } else if (rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) > 16 && nearbyEncampments.length != 0) {
-                                if (rc.senseEncampmentSquare(rc.getLocation())) rc.captureEncampment(RobotType.GENERATOR);
-                                else {
-                                    Direction dir = rc.getLocation().directionTo(nearbyEncampments[0]);
-                                    if (rc.canMove(dir)) rc.move(dir);
-                                    else rc.move(randomDir(rc));
-                                }
-                            } else if (rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) > 16 && nearbyEncampments.length == 0) {
-                                Direction dir = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
-                                if (rc.canMove(dir)) rc.move(dir);
-                                else rc.move(randomDir(rc));
-                            } else { 
-                                // Choose a random direction, and move that way if possible
-                                Direction dir = randomDir(rc);
-                                if(rc.canMove(dir)) {
-                                    rc.move(dir);
-                                    rc.setIndicatorString(0, "Last direction moved: "+dir.toString());
-                                }
-                            }
-                        }
-                    }
-					
-                    if (Math.random()<0.01 && rc.getTeamPower()>5) {
-                        // Write the number 5 to a position on the message board corresponding to the robot's ID
-                        rc.broadcast(rc.getRobot().getID()%GameConstants.BROADCAST_MAX_CHANNELS, 5);
-                    }
-                    rc.yield();
-                }
 
+                                        
+                        // if (Clock.getRoundNum() >= 250){
+                        //     if (Math.random() < .25) {// && rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) < 16) {
+                        //         // Lay a mine 
+                        //         if(rc.senseMine(rc.getLocation())==null)
+                        //             rc.layMine();
+                        //     } else if (rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) > 16 && nearbyEncampments.length != 0) {
+                        //         if (rc.senseEncampmentSquare(rc.getLocation())) rc.captureEncampment(RobotType.GENERATOR);
+                        //         else {
+                        //             Direction dir = rc.getLocation().directionTo(nearbyEncampments[0]);
+                        //             if (rc.canMove(dir)) rc.move(dir);
+                        //             else rc.move(randomDir(rc));
+                        //         }
+                        //     } else if (rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) > 16 && nearbyEncampments.length == 0) {
+                        //         Direction dir = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+                        //         if (rc.canMove(dir)) rc.move(dir);
+                        //         else rc.move(randomDir(rc));
+                        //     } else { 
+                        //         // Choose a random direction, and move that way if possible
+                        //         Direction dir = randomDir(rc);
+                        //         if(rc.canMove(dir)) {
+                        //             rc.move(dir);
+                        //             rc.setIndicatorString(0, "Last direction moved: "+dir.toString());
+                        //         }
+                        //     }
+                        // }
+
+            
+                    // if (Math.random()<0.01 && rc.getTeamPower()>5) {
+                    //     // Write the number 5 to a position on the message board corresponding to the robot's ID
+                    //     rc.broadcast(rc.getRobot().getID()%GameConstants.BROADCAST_MAX_CHANNELS, 5);
+                    // }
+                    rc.yield();
+                }    
                 // End turn
 //                rc.yield();
             } catch (Exception e) {
---
+++ Battlecode/teams/dumbbot/hqCode.java
@@ -0,0 +1,32 @@
+package dumbbot;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.Upgrade;
+
+public class hqCode {
+    private static Direction randomDir(RobotController rc) {
+        Direction dir = Direction.values()[(int)(Math.random()*8)];
+        if(rc.canMove(dir)) return dir;
+        else return randomDir(rc);
+    }
+
+	public static void hqRun(RobotController rc, MapLocation enemyHQ) throws GameActionException {
+		// TODO Auto-generated method stub
+		if (rc.isActive()) {
+			if (rc.getTeamPower() < 50) {
+				if (!rc.hasUpgrade(Upgrade.PICKAXE)) rc.researchUpgrade(Upgrade.PICKAXE);
+				else if(!rc.hasUpgrade(Upgrade.FUSION)) rc.researchUpgrade(Upgrade.FUSION);
+				else rc.researchUpgrade(Upgrade.NUKE);
+			} else {
+			    if (Math.random() < 0.2) rc.spawn(randomDir(rc));
+			    else if (!rc.hasUpgrade(Upgrade.PICKAXE)) rc.researchUpgrade(Upgrade.PICKAXE);
+			    else rc.spawn(randomDir(rc));
+			}
+		}
+		rc.yield();
+	}
+
+}
