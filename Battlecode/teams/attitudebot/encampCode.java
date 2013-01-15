c9de3a6668cfa86fff529d889765f582e28acfb1
Julia Sterling <jsterlin@mit.edu>
2013-01-14 20:11:54 -0500

    encamp

--- Battlecode/teams/attitudebot/encampCode.java
+++ Battlecode/teams/attitudebot/encampCode.java
@@ -1,12 +1,34 @@
 package attitudebot;
 
+import battlecode.common.GameActionException;
+import battlecode.common.GameConstants;
+import battlecode.common.MapLocation;
+import battlecode.common.Robot;
 import battlecode.common.RobotController;
+import battlecode.common.RobotType;
 
 public class encampCode {
 
-	public static void artilleryRun(RobotController rc) {
+	public static void artilleryRun(RobotController rc) throws GameActionException {
 		// TODO Auto-generated method stub
+		if (rc.isActive()) {
+			// if enemy HQ within attack distance: attack it.
+			MapLocation eHQ = rc.senseEnemyHQLocation();
+			if (rc.canAttackSquare(eHQ))
+				rc.attackSquare(eHQ);
+			else
+			{
+				//if artillery towers to attack: attack them
+				
+				//else:
+				Robot[] targets = rc.senseNearbyGameObjects(Robot.class, rc.getLocation(), RobotType.ARTILLERY.attackRadiusMaxSquared, rc.getTeam().opponent());
+				
+			}
+		}
+		// find enemies within shooting radius.
 		
+		// if enemies not empty:
+		//
 	}
 
 	public static void generatorRun(RobotController rc) {
