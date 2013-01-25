eb8592f9b27c55839b87d97c85d4e7938b16f284
pomeroy <jon.danger.pomeroy@gmail.com>
2013-01-24 21:52:01 -0500

    forgot a file

---
+++ Battlecode/teams/sheeptests/soldierCode.java
@@ -0,0 +1,28 @@
+package sheeptests;
+
+import battlecode.common.*;
+
+public class soldierCode {
+    private static int numEnemyMines;
+    private static MapLocation myLoc;
+    private static Team enemy;
+
+    public static void soldierRun(RobotController rc) throws GameActionException {
+        Bug b = new Bug(rc.senseEnemyHQLocation(), rc);
+        
+        while(true){
+            myLoc = rc.getLocation();
+            enemy = rc.getTeam().opponent;
+            numEnemyMines = rc.senseMineLocations(myLoc, 10000, enemy).length;
+        
+            if (rc.isActive()) {
+                if (numEnemyMines <= 3) {
+                    b.go();
+                    rc.yield();
+                } else {
+                    
+            }
+        }
+    }
+}
+                    
